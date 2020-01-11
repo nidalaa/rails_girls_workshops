3c164f0572e3b50fb764f9a85571628b8525f379
Marta Krajewska <krajewska_marta@hotmail.com>
2020-01-11 13:26:09 +0100

    Creating new and create action in IdeasController, adding view for New Idea, fixing routes, setting root path

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,13 +1,35 @@
 class IdeasController < ApplicationController
-  def create
-  end
-
-  def new
-  end
-
   def index
   end
 
+  # GET /ideas/new
+  def new
+    @idea = Idea.new
+  end
+
+
+  # POST /ideas
+  # POST /ideas.json
+  def create
+    @idea = Idea.new(idea_params)
+
+    respond_to do |format|
+      if @idea.save
+        format.html { redirect_to @idea, notice: 'Idea was successfully created.' }
+        format.json { render :show, status: :created, location: @idea }
+      else
+        format.html { render :new }
+        format.json { render json: @idea.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def show
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:name, :description, :picture)
   end
 end
