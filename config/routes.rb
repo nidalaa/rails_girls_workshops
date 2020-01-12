42b62f9ca0f038601e9f28f692d204be80282080
korneliacieslik <korneliacieslik08@gmail.com>
2020-01-12 12:42:00 +0100

    Add edit and update action to ideas

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'ideas#index'
-  resources :ideas, only: %i[index new create show]
+  resources :ideas, only: %i[index new create show edit update]
 end
